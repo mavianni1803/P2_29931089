5c8521c704808c4fc32c8756a5181ebb4a17a9fa
mavianni1803 <166150298+mavianni1803@users.noreply.github.com>
2024-06-13 18:25:17 +0200

    Delete public directory

--- public/javascripts/recapchat.js
+++
@@ -1,8 +0,0 @@
-function verificarCaptcha(event) {
-    let response = grecaptcha.getResponse();
-    if (response.length === 0) {
-        console.log("Fallo en la recolección de datos. Completar Captcha.");
-        event.preventDefault();
-    }
-}
-document.getElementById("form").addEventListener("submit", verificarCaptcha);
